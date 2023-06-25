ada3108fb74150bceda8866645e6d5f3c580f3a2
Derek Nguyen <derek.nguyen02@gmail.com>
2023-06-25 15:25:56 -0700

    Updated navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,8 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    MyShop
-                </Link>
+                <div className="navbar-logo">Add Logo Here</div>
                 <ul className="nav-menu">
-                    <li className="nav-item">
-                        <Link to="/products" className="nav-link">
-                            Products
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/cart" className="nav-link">
-                            Cart
-                        </Link>
-                    </li>
                     <li className="nav-item">
                         <Link to="/account/login" className="nav-link">
                             Login
@@ -31,10 +19,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-{/* <nav id='navbar'>
-        <div className='links'>
-          <Link to="/account/login">Login</Link>
-        </div>
-      </nav> */}
