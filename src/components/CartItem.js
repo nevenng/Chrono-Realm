7bd2ca851899b0b70d459c7f6f26c0a56a63efda
Derek Nguyen <derek.nguyen02@gmail.com>
2023-06-28 15:49:32 -0400

    added CartSummary and CartItem components

---
+++ src/components/CartItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CartItem = () => {
+    return (
+        <tr>
+            <td>Product Name 1</td>
+            <td>$10</td>
+            <td>2</td>
+            <td>$10</td>
+        </tr>
+    )
+}
+
+export default CartItem;
--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,6 @@ export { default as ProductListPage } from './ProductListPage';
 export { default as ProductListItem } from './ProductListItem';
 export { default as Logout } from './Logout';
 export { default as MyOrders} from './MyOrders';
-export { default as MyItems} from './MyItems'
+export { default as MyItems} from './MyItems';
+export { default as CartItem } from './CartItem';
+export { default as CartSummary } from './CartSummary'
