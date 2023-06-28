2e2ecc60b8b9db13de51ebd7784ae4002707c399
Derek Nguyen <derek.nguyen02@gmail.com>
2023-06-28 16:12:12 -0400

    added calculation of subtotal in cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CartItem = () => {
+    const [itemQuantity, setItemQuantity] = useState(1)
+
+    const handleQuantityClicker = (event) => {
+        setItemQuantity(parseInt(event.target.value,10))
+    }
+
     return (
         <tr>
             <td>Product Name 1</td>
             <td>$10</td>
-            <td>2</td>
-            <td>$10</td>
+            <td>
+                <input
+                    type="number"
+                    value={itemQuantity}
+                    onChange={handleQuantityClicker}
+                    >
+                </input>
+            </td>
+            <td>${itemQuantity * 10}</td>
         </tr>
     )
 }
