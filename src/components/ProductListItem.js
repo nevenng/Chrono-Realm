81822886992b8e459396a76baac1b725ff3f130c
Derek Nguyen <derek.nguyen02@gmail.com>
2023-06-30 13:49:58 -0400

    completed frontend component to render all products

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom"
 const ProductListItem = (props) => {
     // Will need to access productIds from props and dynamically set the :productId 
     const { product } = props;
-    console.log(product)
 
     return (
         <div className="plp-item">
-            <Link to="/products/:productId">
-                {/* <img src="https://www.mvmt.com/dw/image/v2/BDKZ_PRD/on/demandware.static/-/Sites-mgi-master/default/dw3b08217b/images/products/28000088_fr.jpg?sw=512&sh=640&q=85"></img> */}
+            <Link to={`/products/${product.prodid}`}>
                 <img src={`${product.prodimg}`}></img>
             </Link>
             <h3>{product.prodmodelname}</h3>
