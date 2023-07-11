fe004b5a1f5947652e085a65486e1727618718c5
Chris Perez <chrisnaperez@gmail.com>
2023-07-10 22:19:38 -0700

    Remove from button is functional

--- src/components/RemoveFromCart.js
+++ src/components/RemoveFromCart.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import { handleRemoveFromCart } from '../axios-services';
+import React, { useState, useEffect } from "react";
+import { handleRemoveFromCart, fetchProductsCart } from '../axios-services';
 
-const RemoveFromCart = ({ userToken, cartProdId }) => {
-    const [isRemoved, setRemoved] = useState(false);
+const RemoveFromCart = (props) => {
+    const { product, user, sessionId, fetchProductsCart, setProducts } = props;
 
     const removeFromCart = async () => {
         try {
-            const result = await handleRemoveFromCart(userToken, cartProdId);
-            alert('Item has been removed from your cart!');
-            setRemoved(true);
+            const result = await handleRemoveFromCart(product.cartid, product.cartprodid);
+            // alert('Item has been removed from your cart!');
+            const products = await fetchProductsCart(user?.id || null, sessionId);
+            setProducts(products);
             return result;
         } catch (err) {
             console.error(err);
